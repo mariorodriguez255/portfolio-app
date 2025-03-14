9468f93ce7974309188b83a3548d3f3139f1a996
Mario Rodriguez <mario.rodriguez.2006@protonmail.com>
2025-03-14 11:51:24 +0100

    Mejorar la navegación y el diseño de las páginas; agregar enlaces y estilos de transición.

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+      extend: {
+        transitionProperty: {
+          'colors': 'background-color, border-color, color, fill, stroke',
+        },
+        transitionDuration: {
+          '300': '300ms',
+        },
+        transitionTimingFunction: {
+          'ease-in-out': 'ease-in-out',
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
